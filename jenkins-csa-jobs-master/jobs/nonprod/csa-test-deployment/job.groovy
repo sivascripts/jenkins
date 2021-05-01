815af4ee55299c77f9a91dfff604fc8ebb9d01bd
Balusu <SivaVeeraDurgaPrakash.Balusu@sabre.com>
2021-05-01 10:37:30 +0530

    modified backend

--- jenkins-csa-jobs-master/jobs/nonprod/csa-test-deployment/job.groovy
+++ jenkins-csa-jobs-master/jobs/nonprod/csa-test-deployment/job.groovy
@@ -22,3 +22,23 @@ pipelineJob('csa-terraform-deploy') {
     }
   }
 }
+
+pipelineJob('csa-terraform-destroy') {
+  description('Deploy Pipeline')
+  logRotator(10, 10)
+  parameters {
+   choiceParam('gitCreds', [gitCreds], 'git credntials id')
+   choiceParam('gitUrl', [gitDeployRepo], 'Git Url')
+   stringParam('gitBranch', 'master', 'Git branch name')
+   stringParam('terraformaccounttype', 'nonprod', 'Terraform account')
+   choiceParam('terraformenv', ['dev','test','pre'],'Environment that will the stacks will be deployed')
+   booleanParam('tfPlanLinuxInstance', false, 'terraform plan on ASG_AISServer')
+   booleanParam('terraformDestroy', false, 'terraform APPLY on above selected terraform plans')
+  }
+  definition {
+    cps {
+      script(readFileFromWorkspace('jenkins-csa-jobs-master/pipelines/csa-test-deployment/destroy/destroy.groovy'))
+	    sandbox()
+    }
+  }
+}
